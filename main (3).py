6a01d83d5b8f5294966dfd30b7ab9a313e26538b
KeepMySecret <158459827+KeepMySecret@users.noreply.github.com>
2024-02-01 14:34:32 +0300

    01.02.2024 задания с 1 по 5

---
+++ main (3).py
@@ -0,0 +1,28 @@
+#coding: utf-8
+#Задача 1
+x = int(input("Введите x = "))
+y = 7*x+5
+print("Ответ y =", y)
+#Задача 2
+a = int(input("Введите a = "))
+b = a*a*a*a
+print("Ответ b =", b)
+#Задача 3
+Пи = 3.14
+r = int(input("Введите r = "))
+C = 2*Пи*r
+print("Ответ C =", C)
+#Задача 4
+Пи = 3.14
+d = int(input("Введите d = "))
+S = Пи*d*d/4
+print("Ответ S =", S)
+#Задача 5
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+a1 = a
+b1 = b1
+c*c1 = a*a1+b*b1
+c1 = c
+c2 = c*c1*0.5
+print("Ответ c2 =", c2)
